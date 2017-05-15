ce2b7abfef637aa476b87b4f6b3ba5f4f9c89c81
Stephen <stephen@daytamarketing.com>
2017-05-15 18:22:04 -0500

    Added JavaScript.

---
+++ js/funWithJavaScript.js
@@ -0,0 +1,29 @@
+// Some JavaScript concepts are below...
+// NOTE: Variables defined here are in the GLOBAL SCOPE (ahhhhhhh)
+
+// console.log('MATH');
+// var twoPlusThree = 2 + "3"; // Huh what?
+// console.log("2 + \"3\" is " + twoPlusThree); // What do you think will be printed out?
+// console.log("2 + 3 is " + (2 + 3));
+// console.log(2 + 3 * 2 - 6 / 2 + Math.pow(2, 3)); // Order of operations...are they the same as you're used to?
+// console.log(typeof(twoPlusThree));
+// 
+// // Object literals
+// var obj = { hi: 'hi', hello: 'hello' };
+// console.log('OBJECT LITERALS')
+// console.log(obj['hi']);
+// console.log(obj.hi);
+//
+// // Type comparisons
+// console.log('TYPE COMPARISONS')
+// console.log(1 == "1");
+// console.log(1 === "1");
+// console.log(typeof(1), typeof("1"));
+//
+// console.log('FUNCTIONS')
+// var myFunc = function() {
+//   console.log('look at me!');
+// };
+// myFunc(); // How many times will we see 'look at me!'?
+// myFunc = 2;
+// console.log(myFunc); // What happens here?
---
+++ js/index.js
@@ -0,0 +1,21 @@
+// This wraps all of my code in what's called a closure, which encapsulates these variables inside this file's scope.
+(function() {
+  function addListeners() {
+    document.querySelector('#nameForm').addEventListener('submit', function(event) {
+      event.preventDefault();
+      var name = event.currentTarget.nameInput.value;
+      var node = document.createElement("LI");
+      var textNode = document.createTextNode(name);
+      node.appendChild(textNode);   
+      document.querySelector('#dogNames').appendChild(node);
+      event.currentTarget.nameInput.value = '';
+    });
+  }
+
+  // When stuff is loaded on screen...
+  document.addEventListener("DOMContentLoaded", function(event) {
+    // Call this function.
+    addListeners();
+  });
+
+})();
