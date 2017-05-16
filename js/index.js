d34d87b1f5d3fcd3d31ec4661d67548395c65e6f
Stephen <stephen@daytamarketing.com>
2017-05-15 21:30:55 -0500

    Added jquery example

--- js/index.js
+++ js/index.js
@@ -1,21 +1,29 @@
 // This wraps all of my code in what's called a closure, which encapsulates these variables inside this file's scope.
-(function() {
-  function addListeners() {
-    document.querySelector('#nameForm').addEventListener('submit', function(event) {
-      event.preventDefault();
-      var name = event.currentTarget.nameInput.value;
-      var node = document.createElement("LI");
-      var textNode = document.createTextNode(name);
-      node.appendChild(textNode);   
-      document.querySelector('#dogNames').appendChild(node);
-      event.currentTarget.nameInput.value = '';
-    });
-  }
+// (function() {
+//   function addListeners() {
+//     document.querySelector('#nameForm').addEventListener('submit', function(event) {
+//       event.preventDefault();
+//       var name = event.currentTarget.nameInput.value;
+//       var node = document.createElement("LI");
+//       var textNode = document.createTextNode(name);
+//       node.appendChild(textNode);
+//       document.querySelector('#dogNames').appendChild(node);
+//       event.currentTarget.nameInput.value = '';
+//     });
+//   }
+//
+//   // When stuff is loaded on screen...
+//   document.addEventListener("DOMContentLoaded", function(event) {
+//     // Call this function.
+//     addListeners();
+//   });
+//
+// })();
 
-  // When stuff is loaded on screen...
-  document.addEventListener("DOMContentLoaded", function(event) {
-    // Call this function.
-    addListeners();
+$(document).ready(function() {
+  $('#nameForm').onClick(function(e) {
+    e.preventDefault();
+    $('#dogNames').append('<li>' + e.currentTarget.nameInput.value + '</li>');
+    e.currentTarget.nameInput.value = '';
   });
-
-})();
+});
